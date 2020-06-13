811463bd74a667db18d84a988c90b7763ff3cfb0
bpo-s.cano <bpo-s.cano@contentobpsrappi.onmicrosoft.com>
2020-06-12 23:54:10 -0500

    Add usewr repository put test

--- src/test/user.repository.test.ts
+++ src/test/user.repository.test.ts
@@ -23,7 +23,7 @@ describe('UserRepository', () => {
   describe("getUsers", () => {
     it("should get all users from db", async () => {
       const userRepository = new UserRepository();
-      const users = await userRepository.getAll(Enums.FN_GET_USERS);
+      const users = await userRepository.getUsers();
       expect(typeof (users)).to.equal(typeof (Array<User>()));
     })
   });
@@ -33,6 +33,15 @@ describe('UserRepository', () => {
       const userHelper = new UserHelper();
       const values = userHelper.getuserValues(stubValue as User);
       const user = await userRepository.postUser(values.slice(1));
+      expect(typeof (user)).to.equal(typeof (stubValue));
+    })
+  });
+  describe("putUser", () => {
+    it("should update user into db", async () => {
+      const userRepository = new UserRepository();
+      const userHelper = new UserHelper();
+      const values = userHelper.getuserValues(stubValue as User);
+      const user = await userRepository.putUser(values);
       expect(typeof (user)).to.equal(typeof (stubValue));
     })
   });
